a439e106ff44d5d09691f7c9783df7d8050ce75f
Shreeja9841 <shreeja.manandhar@cgnet.com.np>
2023-05-13 22:48:28 +0545

    progress

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div>
+        <Router>
+            <Routes>
+                <Route/>
+            </Routes>
+        </Router>
+    </div>
+  )
+}
+
+export default App
